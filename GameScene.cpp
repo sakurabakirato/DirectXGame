e6c7f45f50e0a7bdf4c3bb934430cd8f4989d894
sakurabakirato <k024g0054@g.neec.ac.jp>
2025-04-16 12:45:55 +0900

    画像を描画

--- GameScene.cpp
+++ GameScene.cpp
@@ -2,9 +2,18 @@
 
 using namespace KamataEngine;
 
-void GameScene::Intialize() 
-{
+GameScene::~GameScene() 
+{ 
+	delete model_; 
+}
 
+void GameScene::Intialize() { 
+	textureManager_ = TextureManager::Load("player_4.png"); 
+
+	model_ = Model::Create();
+
+	worldTransform_.Initialize();
+	camera_.Initialize();
 }
 
 void GameScene::Update() 
@@ -13,6 +22,13 @@ void GameScene::Update()
 }
 
 void GameScene::Draw() 
-{
+{ 
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance(); 
 
+	Model::PreDraw(dxCommon->GetCommandList());
+
+	model_->Draw(worldTransform_, camera_, textureManager_);
+
+	Model::PostDraw();
 }
+
--- GameScene.h
+++ GameScene.h
@@ -5,11 +5,21 @@
 class GameScene 
 {
 public:
+	~GameScene();
 	//初期化
 	void Intialize();
 	//更新
 	void Update();
 	//描画
 	void Draw();
+
+private:
+	uint32_t textureManager_ = 0;
+
+	KamataEngine::Model* model_ = nullptr;
+
+	KamataEngine::WorldTransform worldTransform_;
+
+	KamataEngine::Camera camera_;
 
 };
